f08925baf41ecdec926fdd3852a4efb8f219f523
dinesh <ghantasaladinesh6@gmail.com>
2025-04-01 22:39:49 +0530

    added link to google drive cv

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { NavLink, useLocation } from "react-router-dom"
 import CV from 'src/assets/files/Dinesh_Ghantasala.pdf'
 import DarkModeToggle from "./DarkModeToggle"
+const resumeLink = "https://docs.google.com/document/d/1LlG_DJXBvQdlwcSjVaEgNiNFmyMj2bnd6PfRmma9fdI/edit?usp=sharing"
 const NavBar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
     const location = useLocation()
@@ -49,7 +50,7 @@ const NavBar = () => {
                 </NavLink>
                 <div className="flex lg:order-2 items-stretch  space-x-2 lg:justify-end">
                     <DarkModeToggle />
-                    <a href = {CV} download type="button" className="flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 lg:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href = {resumeLink} target = "_blank" type="button" className="flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 lg:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <span><span className="hidden md:inline-block">Download </span> CV</span>
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-current ml-2" viewBox="0 0 24 24"><path d="M12 5c3.453 0 5.891 2.797 5.567 6.78 1.745-.046 4.433.751 4.433 3.72 0 1.93-1.57 3.5-3.5 3.5h-13c-1.93 0-3.5-1.57-3.5-3.5 0-2.797 2.479-3.833 4.433-3.72-.167-4.218 2.208-6.78 5.567-6.78zm0-2c-4.006 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408-.212-3.951-3.473-7.092-7.479-7.092zm-4 10h3v-4h2v4h3l-4 4-4-4z"/></svg>
                     </a>
